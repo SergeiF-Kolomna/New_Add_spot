039eb09918a8c1bbef02e635fdbe4fa52565ea1b
tursnr <tursnr@yandex.ru>
2023-12-07 16:36:55 +0300

    Добавьте файлы проекта.

---
+++ New_Add_spot.py
@@ -0,0 +1,109 @@
+import cv2
+import numpy as np
+
+# Global variables
+drawing = False
+ix, iy = -1, -1
+roi_enlarged = np.zeros((512, 512, 3), np.uint8)
+contour_image = None
+
+# Mouse callback function for drawing on enlarged image
+def draw_red(event, x, y, flags, param):
+    global ix, iy, drawing, roi_enlarged
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.circle(roi_enlarged, (x, y), 15, (0, 0, 255), -1)
+        cv2.imshow('Enlarged ROI', roi_enlarged)
+
+# Mouse callback function for selecting area and drawing rectangles
+def draw_rectangle(event, x, y, flags, param):
+    global ix, iy, drawing, roi_enlarged
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.rectangle(img, (ix-2, iy-2), (x+1, y+1), (0, 255, 0), 2)
+        roi = img[iy:y, ix:x]
+        roi_enlarged = cv2.resize(roi, None, fx=5, fy=5, interpolation=cv2.INTER_CUBIC)
+        cv2.imshow('Enlarged ROI', roi_enlarged)
+
+def red_contour_detect(image):
+    # Чтение изображения
+    #image = cv2.imread(image)  # Замените 'your_image.jpg' на путь к вашему изображению
+
+    # Преобразование изображения из BGR в HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Определение диапазона красного цвета в HSV
+    lower_red = np.array([0, 100, 100])
+    upper_red = np.array([10, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+    lower_red = np.array([160, 100, 100])
+    upper_red = np.array([180, 255, 255])
+    mask2 = cv2.inRange(hsv, lower_red, upper_red)
+
+    # Объединение масок
+    mask = mask1 + mask2
+
+    # Применение морфологических операций для улучшения результата
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+    # Нахождение контуров на изображении
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Отрисовка контуров на исходном изображении
+    cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+
+    # Отображение результатов
+    #cv2.imshow('Original Image', image)
+    #cv2.imshow('Mask', mask)
+    return (contours)
+
+# Load an image
+img = cv2.imread('test2.jpg')
+
+# Create a window, and bind the function to the window
+cv2.namedWindow('Select Area and Draw', cv2.WINDOW_NORMAL)
+cv2.setMouseCallback('Select Area and Draw', draw_rectangle)
+
+while True:
+    cv2.imshow('Select Area and Draw', img)
+    k = cv2.waitKey(1) & 0xFF
+
+    if k == 27:  # Press 'Esc' to exit
+        break
+    elif k == 114:  # Press 'r' to switch to drawing mode
+        cv2.imshow('Enlarged ROI', np.zeros((512, 512, 3), np.uint8))  # Clear the enlarged ROI window
+        cv2.setMouseCallback('Enlarged ROI', draw_red)  # Switch to drawing on the enlarged image
+        while True:
+            cv2.imshow('Enlarged ROI', roi_enlarged)
+            k2 = cv2.waitKey(1) & 0xFF
+            if k2 == 27:  # Press 'Esc' to exit drawing mode
+                # Extract the red channel
+                contour_image = red_contour_detect(roi_enlarged)
+                
+                cv2.drawContours(roi_enlarged, contour_image, -1, (0, 255, 0), 2)
+                cv2.imshow('Outline of Red Image', roi_enlarged)
+                break
+
+        # Switch back to selecting area and drawing rectangles
+        cv2.setMouseCallback('Select Area and Draw', draw_rectangle)
+        
+        for result_contour in contour_image:
+            result_contour[:, :, 0] = result_contour[:, :, 0] * 0.2
+            result_contour[:, :, 1] = result_contour[:, :,  1] * 0.2
+        cv2.drawContours(img, result_contour, -1, (0, 0, 255), 2)
+
+
+cv2.destroyAllWindows()
